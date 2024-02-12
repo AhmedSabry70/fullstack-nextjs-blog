a89f16a7875ca6570bf1c35c789205a16798bd21
Ahmed Sabry <infosabry88@gmail.com>
2024-02-12 02:26:51 +0200

    fix nextauth

--- utils/auth.js
+++ utils/auth.js
@@ -57,6 +57,22 @@ const authOptions = {
       session : {
         strategy:'jwt',
       },
+      callbacks: {
+        session: async ({ session, token }) => {
+          session.id = token.id;
+          session.jwt = token.jwt;
+          return Promise.resolve(session);
+        },
+        jwt: async ({ token, user }) => {
+          const isSignIn = user ? true : false;
+          if (isSignIn) {
+            token.id = user.id;
+            token.jwt = user.jwt;
+          }
+          return Promise.resolve(token);
+        },
+      },
+    
       pages: {
         signIn: '/login',
         /* signOut: '/auth/signout',
